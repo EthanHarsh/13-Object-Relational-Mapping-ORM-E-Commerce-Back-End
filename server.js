fe07d79b28cab77f4d088a7c707dcd19abf1ab0c
Ethan Harsh <ethan@ethanharsh.com>
2021-08-02 17:39:48 +0000

    import sqlze/test connect

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { Sequelize } = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+testConnection()
+
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
